52b3adb327404b3118c196103c430ed95627f063
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2024-07-29 01:09:28 +0530

    Added files to repository

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import './ExploreMenu.css'
-import { menu_list } from '../../assets/assets'
+import React from 'react';
+import './ExploreMenu.css';
+import { menu_list } from '../../assets/assets';
 
-
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore our menu</h1>
-        <p className='explore-menu-text'> choose from a diverse menu featuring a delectable array of dishes.Our mission is to satisfy your cravings and elevate your dining rxperience.one deliceous meal at a time.</p>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>
+        Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.
+      </p>
       <div className='explore-menu-list'>
-        {menu_list.map((item, index) => {
-            return (
-            <div key={index}className='explore-menu-list-item' >
-                <img src={item.menu_image} alt="" />
-                <p>{item.menu_name}</p>
-                </div>
-
-            )
-
-        })}
-
-<hr/>
-
+        {menu_list.map((item, index) => (
+          <div
+            onClick={() => setCategory(prev => (prev === item.menu_name ? "All" : item.menu_name))}
+            key={index}
+            className='explore-menu-list-item'
+          >
+            <img
+              className={category === item.menu_name ? "active" : ""}
+              src={item.menu_image}
+              alt={item.menu_name}
+            />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+        <hr/>
       </div>
     </div>
-  )
+  );
 }
 
-export default ExploreMenu
+export default ExploreMenu;
