de622c38947baef3e76632860bd6a435d75c7125
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2024-07-31 01:23:32 +0530

    Added files to repository

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
-
+import LoginPopup from './components/LoginPopup/LoginPopup';
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
-    <div className='app'>
-      <Navbar/>
-      
-      <Routes>
-       
-        <Route path="/" element={<Home />} />
-        <Route path="/placeorder" element={<PlaceOrder />} />
-        <Route path="/cart" element={<Cart/>} />
-      </Routes>
-     
-    </div>
-    <Footer/>
+      {showLogin && <LoginPopup />}
+      <Navbar setShowLogin={setShowLogin} />
+      <div className='app'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,8 @@ import './Navbar.css';
 import { assets } from '../../assets/assets';
 import {Link}from 'react-router-dom'
 
-const Navbar = () => {
+
+const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home");
 
   return (
@@ -52,7 +53,7 @@ const Navbar = () => {
         <img src={assets.basket_icon} alt=""  />
         <div className='dot'></div>
        </div>
-        <button>sign in</button>
+        <button onClick={()=>setShowLogin(true)}>sign in</button>
         </div>
        
     </div>
