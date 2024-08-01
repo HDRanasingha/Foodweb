c502e7ff649e39292382e3285997e392e1d11646
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2024-08-01 15:05:38 +0530

    Added files to repository

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react';
-import './Cart.css';
-import { StoreContext } from '../../context/StoreContext';
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
   const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
-    <div className='cart'>
-      <div className='cart-items'>
-        <div className='cart-items-title'>
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
@@ -18,23 +18,59 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-       {food_list.map((item,index)=>{
-        if(cartItems[item._id]>0){
-          return(
-            <div className='cart-items-title cart-items-item'>
-            <img src={item.image} alt='' />
-            <p>{item.name}</p>
-            <p>${item.price}</p>
-            <p>{cartItems[item._id]}</p>
-            <p>${cartItems[item._id]*item.price}</p>
-            <p>x</p>
-            </div>
-          )
-        }
-       })}
-         
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt={item.name} />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${cartItems[item._id] * item.price}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
       </div>
-    </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr /></div>
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+            <b>Total</b>
+            <b>{0}</b>
+          </div> 
+          <button>PROCEED TO CHEKOUT</button>
+        </div>
+       
+      
+      <div className="cart-promocode">
+        <div>
+  <p>If you have a promo code, enter it here</p>
+  <div className="cart-promocode-input">
+    <input type="text" placeholder="Promo code" />
+    <button>Submit</button>
+  </div>
+  </div>
+</div>
+</div>
+   </div> 
   );
 };
 
